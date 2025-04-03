d2de9f826d8d8ae07f43bfd31ab817dd7b9df158
Vaibhav Mall <mallvaibhav010@gmail.com>
2025-04-03 15:32:54 +0200

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project for Info Ret. System",
+    version="0.0.0",
+    author="Bappy",
+    author_email="boktiar@ineuron.ai",
+    packages=find_packages(),
+    install_requires=[],
+)
