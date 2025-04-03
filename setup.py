f9b6646209b4d9960e62692c45c6bd95ae684b7c
Vaibhav Mall <mallvaibhav010@gmail.com>
2025-04-03 20:52:41 +0200

    Completed

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Generative AI Project for Info Ret. System",
+    name="Generative AI Project",
     version="0.0.0",
-    author="Bappy",
-    author_email="boktiar@ineuron.ai",
+    author="Vaibhav",
+    author_email="mallvaibhav010@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
